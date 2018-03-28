c0b9955202363b6b69ff6991eb52239c963aa4c7
norshtein <t-tosi@microsoft.com>
2018-03-28 10:00:22 +0000

    Migrate to mail resource

--- send-email/task.sh
+++ send-email/task.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
-ls -al
-pwd
-
-debconf-set-selections <<< "postfix postfix/mailname string 'sitongyao.me'"
-debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-apt-get update 
-apt-get install mailutils -y
-
-echo "==========================Sending Mail================================="
-RECEIVER="t-tosi@microsoft.com"
-
-cat > info.txt <<EOF
+echo "Run CATs Failure" > email-out/${OUTPUT_SUBJECT_FILE_NAME}
+cat >email-out/${OUTPUT_BODY_FILE_NAME} <<EOF
 Running pipeline failed.Please check your pipeline for detailed information.
-
 Pipeline failed time:
 EOF
 
-echo $(TZ='utc-8' date) >> info.txt
-cat info.txt
-echo $RECEIVER
-cat info.txt | mail -s "Running pipeline failed" $RECEIVER 
-
-echo "==========================Sending Success=============================="
+echo $(TZ='utc-8' date) >>email-out/${OUTPUT_BODY_FILE_NAME}
