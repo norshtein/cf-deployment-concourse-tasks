0b60ec05d714b6bf757b1d9a78a574c10b4457fe
norshtein <t-tosi@microsoft.com>
2018-03-28 08:46:23 +0000

    Update send email

--- send-email/task.sh
+++ send-email/task.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+ls -al
+pwd
+
+debconf-set-selections <<< "postfix postfix/mailname string 'sitongyao.me'"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+apt-get update 
+apt-get install mailutils -y
+
+echo "==========================Sending Mail================================="
 RECEIVER="t-tosi@microsoft.com"
 
 cat > info.txt <<EOF
@@ -7,4 +18,8 @@ Pipeline failed time:
 EOF
 
 echo $(TZ='utc-8' date) >> info.txt
+cat info.txt
+echo $RECEIVER
 cat info.txt | mail -s "Running pipeline failed" $RECEIVER 
+
+echo "==========================Sending Success=============================="
